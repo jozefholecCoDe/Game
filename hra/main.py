11edf5067f8f635b2512d6227e6a2d570a3b8d99
Jozef <jozefholeccode@gmail.com>
2026-07-29 15:01:34 +0200

    vytvorenie menu hry

--- hra/main.py
+++ hra/main.py
@@ -5,18 +5,27 @@ rage = 0
 kocka = 6
 utokCelkom = utok + kocka
 trieda = "Warrior"
+hraBezi = True
+while hraBezi:
+    print ("1 - Nová hra /n2 - O hre /n3 - Koniec")
+    volba = input ("<")
+    if volba == "1":
+        print ("==============================")
+        print ("         Syn Kováča           ")
+        print ("==============================")
 
-print ("==============================")
-print ("         Syn Kováča           ")
-print ("==============================")
+        meno = input ("Zadaj meno:")
 
-meno = input ("Zadaj meno:")
+        print (f"Vitaj {meno}!")
+        print (f"Trieda: {trieda}")
+        print (f"❤️  HP:        {hp}")
+        print (f"⚔️  Útok:      {utokCelkom}")
+        print (f"🛡️  Obrana:    {obrana}")
+        print (f"🔥 Rage:       {rage}")
+        print (f"{2+3}")
+        print ("{2+3}")
+    elif volba == "2":
+        print ("Zatial nič")
+    elif volba == "3":
+        hraBezi = False
 
-print (f"Vitaj {meno}!")
-print (f"Trieda: {trieda}")
-print (f"❤️  HP:        {hp}")
-print (f"⚔️  Útok:      {utokCelkom}")
-print (f"🛡️  Obrana:    {obrana}")
-print (f"🔥 Rage:       {rage}")
-print (f"{2+3}")
-print ("{2+3}")
