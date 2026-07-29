010827093c87d029d42ebd1e75b152b1502ebe04
Jozef <jozefholeccode@gmail.com>
2026-07-29 14:48:39 +0200

    ukoncenie lekcie 01

--- hra/main.py
+++ hra/main.py
@@ -2,6 +2,8 @@ hp = 30
 utok = 5
 obrana = 2
 rage = 0
+kocka = 6
+utokCelkom = utok + kocka
 trieda = "Warrior"
 
 print ("==============================")
@@ -13,6 +15,8 @@ meno = input ("Zadaj meno:")
 print (f"Vitaj {meno}!")
 print (f"Trieda: {trieda}")
 print (f"❤️  HP:        {hp}")
-print (f"⚔️  Útok:      {utok}")
+print (f"⚔️  Útok:      {utokCelkom}")
 print (f"🛡️  Obrana:    {obrana}")
 print (f"🔥 Rage:       {rage}")
+print (f"{2+3}")
+print ("{2+3}")
