f2642c67a1753ade2307c364b1ccbb890b7ee58b
Jozef <jozefholeccode@gmail.com>
2026-07-29 14:02:42 +0200

    vytvorenie uvodnej obrazovky

---
+++ hra/main.py
@@ -0,0 +1,18 @@
+hp = 30
+utok = 5
+obrana = 2
+rage = 0
+trieda = "Warrior"
+
+print ("==============================")
+print ("         Syn Kováča           ")
+print ("==============================")
+
+meno = input ("Zadaj meno:")
+
+print (f"Vitaj {meno}!")
+print (f"Trieda: {trieda}")
+print (f"❤️  HP:        {hp}")
+print (f"⚔️  Útok:      {utok}")
+print (f"🛡️  Obrana:    {obrana}")
+print (f"🔥 Rage:       {rage}")
